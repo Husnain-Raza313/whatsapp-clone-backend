dbfa37358af5be8586f65ed142d83fa29ffda2f5
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-01-04 18:46:31 +0500

    Models: Added Validations

--- app/models/chat_room_message.rb
+++ app/models/chat_room_message.rb
@@ -1,4 +1,6 @@
 class ChatRoomMessage < ApplicationRecord
+  validates :body, presence: true
+
   belongs_to :chat_room_participant
   belongs_to :chat_room
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :username, :name, :phone_number, :password, presence: true
+  validates :phone_number, uniqueness: true
 end
