6198f053a488913dd57be5726185bc1f74387b33
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-01-04 15:34:59 +0500

    Application: Added ChatRoom Model and Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_04_101716) do
+ActiveRecord::Schema.define(version: 2023_01_04_102151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
