a8d84708d84b9a77442b78c2c0f6b556378ad1b6
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-01-06 19:44:52 +0500

    Application: Added Routes for Chat Room messages controller and refactored some code

--- app/controllers/api/v1/chat_room_messages_controller.rb
+++ app/controllers/api/v1/chat_room_messages_controller.rb
@@ -1,32 +1,43 @@
-class ChatRoomMessagesController < ApplicationController
-  before_action :set_users, only: [:create]
+module Api
+  module V1
+    class ChatRoomMessagesController < ApplicationController
+      before_action :set_users, only: [:create]
 
-  def index
-    @chat_room_messages = ChatRoomMessages.where(chat_room_id: params[:chat_room_id]).order("created_at DESC") if params[:chat_room_id].present?
-    if @chat_room_messages.present?
-      render json: @chat_room_messages
-    else
-      render json: 'You have no conversation with this user'
-    end
-  end
+      def index
+        @chat_room_messages = ChatRoomMessages.where(chat_room_id: params[:chat_room_id]).order("created_at DESC") if params[:chat_room_id].present?
+        if @chat_room_messages.present?
+          render json: @chat_room_messages
+        else
+          render json: 'You have no conversation with this user'
+        end
+      end
 
-  def create
-    @message, @chat_room = ChatSearchService.new(user1: @user1, user2: @user2, message_params: params[:message_params])
-    if @message.save
-      ChatRoomChannel.broadcast_to(@message, @chat_room)
-      render json: @message
-    else
-      render json: @message.errors.full_messages, status: 422
-    end
+      def create
+        @message, @chat_room = ChatSearchService.new(user1: @user1, user2: @user2, message_params: message_params)
+        if @message.save
+          ChatRoomChannel.broadcast_to(@message, @chat_room)
+          render json: @message
+        else
+          render json: @message.errors.full_messages, status: 422
+        end
 
-  end
+      end
 
-  private
+      private
 
-  def set_users
-    if params[:user_id].present? && params[:phone_number].present?
-      @user1 = User.find_by(id: params[:user_id])
-      @user2 = User.find_by(phone_number: params[:phone_number])
+      def message_params
+        params.require(:chat_room_message).permit(:body)
+      end
+
+      def set_users
+        byebug
+        if params[:user_id].present? && params[:phone_number].present?
+          @user1 = User.find_by(id: params[:user_id])
+          @user2 = User.find_by(phone_number: params[:phone_number])
+        else
+          render json: 'Please enter a valid phone number'
+        end
+      end
     end
   end
 end
--- app/services/chat_search_service.rb
+++ app/services/chat_search_service.rb
@@ -5,11 +5,13 @@ class ChatSearchService < ApplicationService
     @user1 = args[:user1]
     @user2 = args[:user2]
     @message_params = args[:message_params]
+    byebug
   end
 
   def search
-    phone_numbers = [@user1.phone_number, @user2.phone_number].sort!
+    phone_numbers = [user1.phone_number, user2.phone_number].sort!
     chat_room = ChatRoom.find_by(name: phone_numbers.join('-'))
+    chat_room_particpant1 = ChatRoomParticipant.create(user_id: user1.id, chat_room_id: chat_room.id) if chat_room.present?
     chat_room, chat_room_particpant1 = create_chat_and_participants unless chat_room.present?
     message = chat_room_particpant1.chat_room_messages.new(message_params)
     message.chat_room_id = chat_room.id
