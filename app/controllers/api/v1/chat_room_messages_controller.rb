17a365131bd87ce1e5415eb30aab2b83ce2828ad
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2023-02-28 02:51:56 +0500

    Application: Added Create Chat Search Service

--- app/controllers/api/v1/chat_room_messages_controller.rb
+++ app/controllers/api/v1/chat_room_messages_controller.rb
@@ -3,12 +3,12 @@
 module Api
   module V1
     class ChatRoomMessagesController < ApplicationController
-      before_action :set_users, only: %i[create index]
+      before_action :set_users, only: %i[create index show]
       before_action :authorize
 
       def index
-        phone_numbers = [@user1.phone_number.last(3), @user2.phone_number.last(3)].sort!
-        chat_room_id = ChatRoom.find_by_phone(phone_numbers)
+        # chat_room_id = ChatRoom.find_by_phone(phone_numbers.join('-'))
+        chat_room_id = ChatRoom.where(name: chat_room_name).pluck(:id)[0]
         if chat_room_id.present?
           sender = ChatRoomParticipant.find_by_user_and_chat_ids(@user1.id, chat_room_id)
           @chat_room_messages = ChatRoomMessage.find_messages(chat_room_id)
@@ -16,6 +16,37 @@ module Api
         else
           render json: { message: 'You have no conversation with this user' }
         end
+      end
+
+      def show
+        byebug
+        # chat_room_id = ChatRoom.where(name: phone_numbers).pluck(:id)[0]
+        chat_room_id, message = CreateChatService.new(sender: @user1, receiver: @user2,
+          chat_room_name: chat_room_name).search_chat_room
+
+        if chat_room_id.blank?
+          render json: { message: message }, status: :unprocessable_entity
+        else
+          render json: {chat_room_id: chat_room_id }, status: :ok
+        end
+      #   if chat_room_id.blank?
+      #     CreateChatService.new(sender: @user1, receiver: @user2,
+      #       phone_numbers: phone_numbers).create_chat_and_participants
+      #     @chat_room = ChatRoom.new(name: phone_numbers.join('-'), user_id: @user1.id)
+      #   if @chat_room.save
+      #     chat_room_participant1 = ChatRoomParticipant.create(user_id: @user1.id, chat_room_id: @chat_room.id,
+      #                                                       receiver_name: @user2.name)
+      #     chat_room_participant2 = ChatRoomParticipant.create(user_id: @user2.id, chat_room_id: @chat_room.id,
+      #                                                       receiver_name: @user1.name)
+
+      #     render json: {chat_room_id: @chat_room.id }
+      #   else
+      #     render json: { message: @chat_room.errors.full_messages }, status: :unprocessable_entity
+      #   end
+      # else
+      #   render json: {chat_room_id: chat_room }
+      # end
+
       end
 
       def create
@@ -35,6 +66,11 @@ module Api
 
       def message_params
         params.permit(:body)
+      end
+
+      def chat_room_name
+        phone_numbers = [@user1.phone_number.last(3), @user2.phone_number.last(3)].sort!
+        phone_numbers = phone_numbers.join('-')
       end
 
       def set_users
---
+++ app/services/create_chat_service.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class CreateChatService < ApplicationService
+  attr_reader :sender, :receiver, :chat_room_name
+
+  def initialize(**args)
+    @params = args
+    @sender = @params[:sender]
+    @receiver = @params[:receiver]
+    @chat_room_name = @params[:chat_room_name]
+  end
+
+  def search_chat_room
+    byebug
+    chat_room_id = ChatRoom.where(name: chat_room_name).pluck(:id)[0]
+    if chat_room_id.blank?
+      create_chat_and_participants
+    else
+      [chat_room_id, message = nil]
+    end
+  end
+
+  def create_chat_and_participants
+    byebug
+    chat_room = ChatRoom.new(name: chat_room_name, user_id: sender.id)
+    if chat_room.save
+      byebug
+      chat_room_participant1 = ChatRoomParticipant.create(user_id: sender.id, chat_room_id: chat_room.id,
+                                                        receiver_name: receiver.name)
+      chat_room_participant2 = ChatRoomParticipant.create(user_id: receiver.id, chat_room_id: chat_room.id,
+                                                        receiver_name: sender.name)
+
+      [chat_room.id, message = nil]
+    else
+      [chat_room.id = nil, message = chat_room.errors.full_messages ]
+    end
+  end
+end
